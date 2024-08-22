2afee46f0a119a55766112576b0a5cf61e422fd7
feruza17 <feruzamirjalilova767@gmail.com>
2024-08-22 17:39:33 +0500

    ;

--- api/api.go
+++ api/api.go
@@ -25,6 +25,7 @@ func Engine(handler *handlers.Handlers) *gin.Engine {
 	router.POST("/login", handler.LoginUser).Use(middleware.Middleware())
 	router.POST("/forgot-password", handler.ForgotPassword)
 	router.POST("/reset-password", handler.ResetPassword)
+	router.GET("/users", handler.GetAllUsers).Use(middleware.JWTMiddleware())
 
 	user := router.Group("/user").Use(middleware.JWTMiddleware())
 	{
